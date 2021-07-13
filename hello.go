d0ffc2bed3babd2e3c35be34a42ba1f2e98e031d
vvpn9 <moonshake@foxmail.com>
2021-07-13 15:15:12 +0800

    initial commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from Go!")
+}
+
+// if your are using build then,
+// this will generate a binary file that designed for your system
+// and if it is copied to windows, it wont work
+// here, go binary file for ur system
